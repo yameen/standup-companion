68aee0cfdfde2f845bea7b60a69c322248b64c6d
yameen <yameen.rasul@bbc.co.uk>
2015-04-22 01:15:19 +0100

    added ticket flow and number of info update

---
+++ client/js/countdowntimer.js
@@ -0,0 +1,82 @@
+    var timePerTicket;
+    function createTimePerTicketSelector() {
+        $('.standupCompanion__timer').append('<input id="timerSelect" type="text" value="2" name="ticketTime">');
+        $("input[name='ticketTime']").TouchSpin({
+            min: 1,
+            max: 6,
+            step: 1,
+            decimals: 0,
+            booster: false,
+            postfix: 'Minutes'
+        });
+    };
+    function destroyAndClearInnerHtmlOfTimerCircles() {
+        var countDown_timer = $('.standupCompanion__timer',window.parent.document);
+        countDown_timer.TimeCircles().destroy();
+        $('.standupCompanion__timer').empty();
+    };
+    function removeTimerAndReDisplaySelector() {
+        destroyAndClearInnerHtmlOfTimerCircles();
+        createTimePerTicketSelector();
+    };
+    function playWithCurrentTimePerTicket() {
+        destroyAndClearInnerHtmlOfTimerCircles();
+        startTicketCountDownTimer(timePerTicket);
+    };
+    function getTimeSetPerTicketInSeconds(){
+        if($('#timerSelect').length){
+            var inputValue = parseInt($('#timerSelect').val(), 10);
+            timePerTicket = inputValue * 60;
+        }
+        else if (timePerTicket) {
+            timePerTicket = timePerTicket <= 0 ? 45 : timePerTicket;
+        }
+        else {
+            timePerTicket = 45;
+        }
+        
+    };
+    function tickOverToNextTicketOrEnd(){
+        if(isThereANextTicket()) {
+            nextTicket();
+            playWithCurrentTimePerTicket();
+        }
+        else {
+            playingTimer = false;
+            setStatusIndicatorToPlay(false);
+            console.log('finished playing all tickets!');
+        }
+    }
+    function startTicketCountDownTimer(timeInSeconds) {
+        var countDown_timer = $('.standupCompanion__timer',window.parent.document);
+        countDown_timer.data('timer',timeInSeconds || 120);
+        countDown_timer.TimeCircles({ time: { Days: { show:false }, Hours: { show:false }, Minutes: { color: '#4D8DC1' }, Seconds: { color: '#4D8DC1' } } })
+        .addListener(
+            function(unit,value,total) {
+                switch(unit)
+                {
+                    case "Minutes":
+                    if (value<=0)
+                    {
+                        countDown_timer.TimeCircles({time: { Days: {show: false}, Hours: {show:false}, Minutes: {show:false}}, Seconds: {show:true}}).rebuild();
+                    }
+                    break;
+                    case "Seconds":
+                    if(total==59)
+                        countDown_timer.TimeCircles.rebuild();
+                    //({time: { Days: {show: false}, Hours: {show:false}, Minutes: {show:false}}, Seconds: {show:true}}).rebuild();
+                    if (total == 30) {
+                        countDown_timer.data('timer',30);
+                        countDown_timer.TimeCircles({ time: { Days: { show:false }, Hours: { show:false }, Minutes: { color: '#900' }, Seconds: { color: '#900' } } })
+                    }
+                    if (total<=0)
+                    {
+                        countDown_timer.TimeCircles().destroy();
+                        tickOverToNextTicketOrEnd();
+                    }
+                    break;
+                }
+            }
+            );
+    playingTimer = true;
+};
--- client/js/standup.js
+++ client/js/standup.js
@@ -1,46 +1,109 @@
 var tickets;
-var ticketCounter;
+var currentTicketCounter;
+var haveValidNonEmptyData = false;
+var totalNumberOfTickets = 0;
 
-function resetTicketCounter() {
-    ticketCounter = 1;
+function resetcurrentTicketCounter() {
+    currentTicketCounter = 1;
 }
 
 function updateTicketView() {
-    var ticket = tickets.issues[ticketCounter-1];
+    totalNumberOfTickets = tickets.issues.length;
+
+    var ticket = tickets.issues[currentTicketCounter-1];
     $('#summary').html(ticket.fields.summary);
     $('#reporter').html(ticket.fields.reporter.name);
     $('#avatars').attr('src', ticket.fields.reporter.avatarUrls['48x48']);
-    var standupProgress = (((ticketCounter)/tickets.issues.length)*100) + '%';
+    var standupProgress = (((currentTicketCounter)/tickets.issues.length)*100) + '%';
     $('#standupProgress').css('width', standupProgress);
+    $('.header__standupInfo span').text('Ticket number '+ currentTicketCounter +' of '+ totalNumberOfTickets);
+
+    
+}
+
+function setStatusIndicatorToPlay(playing){
+    if(playing){
+        $('.header__statusIndicator').addClass('playingTimer');
+    }
+    else {
+        $('.header__statusIndicator').removeClass('playingTimer');
+    }
+
 }
 
 function loadTickets(project, status, epic) {
     var queryURL = "/LoadTickets?project=" + project + "&status=" + status + "&epic=" + epic;
     $.getJSON(queryURL, function(data){
         tickets = data;
-        resetTicketCounter();
+        validateDataReturned(tickets);
+        resetcurrentTicketCounter();
         updateTicketView();
     });
+}
+
+function validateDataReturned(dataIn) {
+    if(dataIn && (totalNumberOfTickets > 0))
+    {
+        haveValidNonEmptyData = true;
+    }
+    else {
+        haveValidNonEmptyData = false;
+    } 
+}
+
+function ticketsHaveBeenLoaded(){
+    return haveValidNonEmptyData;
+}
+
+function isThereANextTicket()
+{
+    if(ticketsHaveBeenLoaded) {
+        if((currentTicketCounter) >= totalNumberOfTickets) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+function isThereAPreviousTicket()
+{
+    if(ticketsHaveBeenLoaded) {
+        if((currentTicketCounter) <= 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 }
 
 function loadFakeData() {
     $.getJSON("FakeData/", function(data){
         tickets = data;
-        resetTicketCounter();
+        resetcurrentTicketCounter();
+        validateDataReturned(tickets);
         updateTicketView();
     });
 }
 
 function nextTicket() {
-    if (ticketCounter < tickets.issues.length) {
-        ticketCounter++;
+    if (currentTicketCounter < tickets.issues.length) {
+        currentTicketCounter++;
         updateTicketView();
     }
 }
 
 function previousTicket() {
-    if (ticketCounter > 1) {
-        ticketCounter--;
+    if (currentTicketCounter > 1) {
+        currentTicketCounter--;
         updateTicketView();
     }
 }
